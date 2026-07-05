5888f48db71a6b35792d0c5f7a9b085fd954b82c
Anas-AbdelRaoof <anasraoof2012@gmail.com>
2026-07-05 20:21:05 +0300

    Refactor docstring format in run function

--- run.py
+++ run.py
@@ -3,9 +3,7 @@ from file_helpers import argv_length
 
 
 def run():
-    """
-    Connects all parts of code and writes the code in your file
-    """
+    """Connects all parts of code and writes the code in your file"""
 
     argv_length()
     write_ai_generated_code()
